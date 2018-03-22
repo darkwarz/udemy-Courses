2841c24548adbaa8e5d0f62987ac4ed5407b3722
jabier espinal <jabierespinal@gmail.com>
2018-03-22 22:04:46 +0000

    lesson 29 completed

--- MEAN_Zero2Hero/MEAN/api/controllers/hotels.controllers.js
+++ MEAN_Zero2Hero/MEAN/api/controllers/hotels.controllers.js
@@ -1,21 +1,10 @@
-var dbconn = require('../data/dbconnection.js');
-var ObjectId = require('mongodb').ObjectID;
-
-var hotelData = require('../data/hotel-data.json');
+var mongoose = require('mongoose');
+var Hotel = mongoose.model('Hotel');
 
 module.exports.hotelsGetAll = function(req, res) {
 
-  var db = dbconn.get();
-
-  // console.log("db", db);
-
-  console.log('GET the hotels');
-  console.log(req.query);
-
   var offset = 0;
   var count = 5;
-
-  var collection = db.collection('hotels');
 
   if (req.query && req.query.offset) {
     offset = parseInt(req.query.offset, 10);
@@ -25,29 +14,25 @@ module.exports.hotelsGetAll = function(req, res) {
     count = parseInt(req.query.count, 10);
   }
 
-  collection
-    .find()
-    .skip(offset)
-    .limit(count)
-    .toArray(function(err, docs) {
-      console.log("Found hotels", docs.length);
-      res
-        .status(200)
-        .json(docs);
+Hotel
+  .find()
+  .skip(offset)
+  .limit(count)
+  .exec(function(err, hotels) {
+    console.log("Found hotels", hotels.length);
+    res
+      .json(hotels);
   });
 
 };
 
 module.exports.hotelsGetOne = function(req, res) {
-  var db = dbconn.get();
-  var id = req.params.hotelId;
-  var collection = db.collection('hotels');
-  console.log('GET hotelId', id);
+  var hotelId = req.params.hotelId;
+  console.log('GET hotelId', hotelId);
 
-  collection
-    .findOne({
-      _id : ObjectId(id)
-    }, function(err, doc) {
+  Hotel
+    .findById(hotelId)
+    .exec(function(err,doc) {
       res
         .status(200)
         .json(doc);
